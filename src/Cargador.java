a1b7df868d995f8bde8029f73b969867fa3b7622
nyx <nishedcob@gmail.com>
2014-01-05 12:05:53 -0500

    Editar Cargador para que tenga una parte de ayuda desde la consola

--- src/Cargador.java
+++ src/Cargador.java
@@ -12,7 +12,35 @@ import data_en_crypto.*;
  */
 public class Cargador {
     public static void main(String[] args) {
-        //aqui va el metodo principal del clase que queremos cargar:
-        DataEnCrypto.main(args);
+        if(existe("-h", args) || existe('h', args)){
+            //Ayuda desde consola
+            System.out.println("");
+        } else {
+            //aqui va el metodo principal del clase que queremos cargar:
+            DataEnCrypto.main(args);
+        }
+    }
+
+    private static boolean existe(String val, String[] args) {
+        for(String arg : args){
+            if(val == null ? arg == null : val.equals(arg)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static boolean existe(char val, String[] args) {
+        for(String arg : args){
+            if(arg.charAt(0) == '-'){
+                char[] arg_chars = arg.toCharArray();
+                for(char c : arg_chars){
+                    if(c == val){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
