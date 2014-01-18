64704195045a86deb84c9b1543d909b7fb0156ea
nyx <nishedcob@gmail.com>
2014-01-18 16:35:43 -0500

    Implimentacion de Superclase Salida (para flujos de Salida)

--- src/data_en_crypto/flujos/salida/Salida.java
+++ src/data_en_crypto/flujos/salida/Salida.java
@@ -10,11 +10,21 @@
 package data_en_crypto.flujos.salida;
 
 import data_en_crypto.flujos.Flujo;
+import java.io.File;
 
 /**
  *
  * @author nyx
  */
 public class Salida extends Flujo{
+    
+    protected File f = null;
+
+    public Salida() {
+    }
+
+    public Salida(File f) {
+        this.f = f;
+    }
     
 }
