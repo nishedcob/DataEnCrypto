6f73960126b0badd10f32194e7c2677e5ea03c81
nyx <nishedcob@gmail.com>
2014-01-18 16:38:57 -0500

    Implimentacion de S_Texto (Flujo de Salida por medio de consola)

--- src/data_en_crypto/flujos/salida/S_Texto.java
+++ src/data_en_crypto/flujos/salida/S_Texto.java
@@ -11,5 +11,25 @@ package data_en_crypto.flujos.salida;
  * @author nyx
  */
 public class S_Texto extends Salida{
+
+    public S_Texto() {
+        super();
+    }
+    
+    public void imprimir(Object o){
+        System.out.print(o);
+    }
+    
+    public void imprimir(String s){
+        System.out.print(s);
+    }
+    
+    public void imprimirln(Object o){
+        System.out.println(o);
+    }
+    
+    public void imprimirln(String s){
+        System.out.println(s);
+    }
     
 }
