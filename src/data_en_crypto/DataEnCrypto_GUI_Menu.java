c7028f747e3067c27e96c044f93e570a87aa9f15
nyx <nishedcob@gmail.com>
2014-01-26 11:08:19 -0500

    Implimentacion de Menu Principal con Jose Luis ( @joseDG )

--- src/data_en_crypto/DataEnCrypto_GUI_Menu.java
+++ src/data_en_crypto/DataEnCrypto_GUI_Menu.java
@@ -1,6 +1,8 @@
 package data_en_crypto;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by nyx on 1/25/14 at 5:36 PM.
@@ -21,33 +23,60 @@ public class DataEnCrypto_GUI_Menu extends JFrame {
 
     JButton jbSalir;
 
+    DataEnCrypto_GUI_Basico dec_gui_b;
+
+    DataEnCrypto_GUI_Avanzada dec_gui_a;
+
     public DataEnCrypto_GUI_Menu(){
         super("DataEnCrypto");
-        this.setSize(500, 400);
+        this.setSize(225, 280);
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
         jpLayout = new JPanel(null);
         this.setContentPane(jpLayout);
 
-        iiIcono = new ImageIcon("data_en_crypto/imgs/Icon_Medium.png");
-        jlIcono = new JLabel(iiIcono);
-        jlIcono.setBounds(10, 10, 363, 363);
+//        setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon_Small.png")));
+//
+//        iiIcono = new ImageIcon("data_en_crypto/imgs/Icon_Medium.png");
+        jlIcono = new JLabel();
+        jlIcono.setIcon(new javax.swing.ImageIcon(getClass().getResource("/data_en_crypto/imgs/Icon_Small.png")));
+        jlIcono.setBounds(50, 10, 73, 73);
         jpLayout.add(jlIcono);
 
         jlTitulo = new JLabel("Menu Principal");
-        jlTitulo.setBounds(383, 10, 107, 20);
+        jlTitulo.setBounds(50, 83, 107, 20);
         jpLayout.add(jlTitulo);
 
         jbInterfazBasica = new JButton("Interfaz Basica");
-        jbInterfazBasica.setBounds(383, 40, 107, 20);
+        jbInterfazBasica.setBounds(17, 123, 180, 20);
+        jbInterfazBasica.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dec_gui_b = new DataEnCrypto_GUI_Basico();
+            }
+        });
         jpLayout.add(jbInterfazBasica);
 
         jbInterfazAvanzada = new JButton("Interfaz Avanzada");
-        jbInterfazAvanzada.setBounds(383, 70, 107, 20);
+        jbInterfazAvanzada.setBounds(17, 163, 180, 20);
+        jbInterfazAvanzada.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dec_gui_a = new DataEnCrypto_GUI_Avanzada();
+            }
+        });
         jpLayout.add(jbInterfazAvanzada);
 
         jbSalir = new JButton("Salir");
-        jbSalir.setBounds(383, 100, 107, 20);
+        jbSalir.setBounds(17, 203, 180, 20);
+        jbSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//                dispose();
+                System.exit(0);
+            }
+        });
         jpLayout.add(jbSalir);
 
         this.setVisible(true);
