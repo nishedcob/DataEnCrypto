e827983271e72710669b5d8f4b561a4f2730c8a9
nyx <nishedcob@gmail.com>
2014-01-27 14:52:01 -0500

    Implimentacion de Interfaz Basico (Parte 2) de DataEnCrypto [DataEnCrypto_GUI_Basico] (con Jose Luis - @joseDG )

--- src/data_en_crypto/DataEnCrypto_GUI_Basico.java
+++ src/data_en_crypto/DataEnCrypto_GUI_Basico.java
@@ -24,9 +24,29 @@ public class DataEnCrypto_GUI_Basico extends javax.swing.JFrame {
 
     JTextArea jtaSalida;
 
+    JButton jbEntradaEliminar;
+
+    JButton jbEntradaBorrar;
+
+    JButton jbEntradaMostrar;
+
+    JButton jbSalidaEliminar;
+
+    JButton jbSalidaBorrar;
+
+    JButton jbSalidaMostrar;
+
+    JTextField jtfEntrada;
+
+    JButton jbEntradaAbrir;
+
+    JTextField jtfSalida;
+
+    JButton jbSalidaGuardar;
+
     public DataEnCrypto_GUI_Basico() {
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.setSize(600, 500);
+        this.setSize(605, 500);
 
         jpDiseno = new JPanel(null);
         this.setContentPane(jpDiseno);
@@ -66,6 +86,46 @@ public class DataEnCrypto_GUI_Basico extends javax.swing.JFrame {
         jspSalida.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         jspSalida.setEnabled(true);
         jpDiseno.add(jspSalida);
+
+        jbEntradaEliminar = new JButton("Eliminar");
+        jbEntradaEliminar.setBounds(10, 270, 90, 20);
+        jpDiseno.add(jbEntradaEliminar);
+
+        jbEntradaBorrar = new JButton("Borrar");
+        jbEntradaBorrar.setBounds(110, 270, 85, 20);
+        jpDiseno.add(jbEntradaBorrar);
+
+        jbEntradaMostrar = new JButton("Mostrar");
+        jbEntradaMostrar.setBounds(205, 270, 90, 20);
+        jpDiseno.add(jbEntradaMostrar);
+
+        jbSalidaEliminar = new JButton("Eliminar");
+        jbSalidaEliminar.setBounds(310, 270, 90, 20);
+        jpDiseno.add(jbSalidaEliminar);
+
+        jbSalidaBorrar = new JButton("Borrar");
+        jbSalidaBorrar.setBounds(410, 270, 85, 20);
+        jpDiseno.add(jbSalidaBorrar);
+
+        jbSalidaMostrar = new JButton("Mostrar");
+        jbSalidaMostrar.setBounds(505, 270, 90, 20);
+        jpDiseno.add(jbSalidaMostrar);
+
+        jtfEntrada = new JTextField();
+        jtfEntrada.setBounds(10, 300, 185, 20);
+        jpDiseno.add(jtfEntrada);
+
+        jbEntradaAbrir = new JButton("Abrir");
+        jbEntradaAbrir.setBounds(205, 300, 90, 20);
+        jpDiseno.add(jbEntradaAbrir);
+
+        jtfSalida = new JTextField();
+        jtfSalida.setBounds(310, 300, 165, 20);
+        jpDiseno.add(jtfSalida);
+
+        jbSalidaGuardar = new JButton("Guardar");
+        jbSalidaGuardar.setBounds(485, 300, 110, 20);
+        jpDiseno.add(jbSalidaGuardar);
 
         this.setVisible(true);
     }
