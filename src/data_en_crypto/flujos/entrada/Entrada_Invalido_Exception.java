aafb00f91adbe417656d1f81483dfc55ffd5363b
Nyx Neptune Nishedcob <nishedcob@users.noreply.github.com>
2014-02-02 13:32:43 -0500

    Nuevo Tipo de Exception para Configuraciones de Entrada que no son validos.

---
+++ src/data_en_crypto/flujos/entrada/Entrada_Invalido_Exception.java
@@ -0,0 +1,26 @@
+package data_en_crypto.flujos.entrada;
+
+/**
+ * Created by nyx at 1:09 PM on 02 February 2014.
+ */
+public class Entrada_Invalido_Exception extends Exception {
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public Entrada_Invalido_Exception() {
+        super("Error! Configuracion de Entrada es Invalido!");
+    }
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public Entrada_Invalido_Exception(String message) {
+        super(message);
+    }
+}
