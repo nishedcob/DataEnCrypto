61033fb934e12e14babd531a8e850136feee513b
Nyx Neptune Nishedcob <nishedcob@users.noreply.github.com>
2014-02-04 20:23:48 -0500

    Implimentar Nuevo Constructor para Entrada de Texto que tome Datos ya leidos. Escribir documentacion de E_Texto.

--- src/data_en_crypto/flujos/entrada/E_Texto.java
+++ src/data_en_crypto/flujos/entrada/E_Texto.java
@@ -6,17 +6,38 @@
 
 package data_en_crypto.flujos.entrada;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nyx
  */
 public class E_Texto extends Entrada{
 
+    /**
+     * Constructor que guarda todo el data leido en data del flujo.
+     * @param Val_Salida la linea con cual no leimos mas data
+     */
     public E_Texto(String Val_Salida) {
         super();
         data = leerData("", Val_Salida);
     }
 
+    /**
+     * Crea un nuevo flujo de entrada de texto con datos ya leidos.
+     *
+     * @param data   datos para almacenar en este objeto
+     * @param lector el scanner que este entrada debe usar para leer
+     */
+    public E_Texto(String data, Scanner lector) {
+        super(data, lector);
+    }
+
+    /**
+     * Lea datos hasta que llega al valor de salida.
+     * @param Val_Salida la linea con cual no leimos mas data
+     * @return todo el data leido menos la linea que es el valor de salida
+     */
     private String leerData(String data, String Val_Salida) {
         String tmp = lector.nextLine();
         if(tmp == null ? Val_Salida == null : tmp.equals(Val_Salida)){
